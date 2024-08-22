576ebbabe9b2d7e0b034f8df38f9097123260335
PaladuguRenuka <166904796+PaladuguRenuka@users.noreply.github.com>
2024-08-22 13:50:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		this.name = "Renuka"
+		this.age = 25
+	}
+	get(){
+		return this.name
+	}
+	set(value){
+		this.age = value
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log(`${this.name} is studying`)
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+		console.log(`${this.name} is teaching`)
+	}
+}
+const student = new Student('Alice', 20);
+student.study(); // Output: Alice is studying
+
+const teacher = new Teacher('Bob', 45);
+teacher.teach(); // Output: Bob is teaching
+
+// Demonstrating the setter for age
+student.age = 21;
+console.log(student.age);
 
 // Do not change the code below this line
 window.Person = Person;
